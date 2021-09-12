5f57598461652d4ba6c53e0d8153f39b81600976
Dmitrii Bormotov <squier7@gmail.com>
2021-09-12 09:01:45 +0500

    add more auth tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+const { addCommands } = require('./helpers/setup.helper');
+
 exports.config = {
     specs: [
         './specs/**/*.spec.js'
@@ -27,5 +29,9 @@ exports.config = {
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
-    }
+    },
+
+    before: async function (capabilities, specs, browser) {
+        await addCommands();
+    },
 }
